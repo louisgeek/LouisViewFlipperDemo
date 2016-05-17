939e763989118888127f38517881a06bdafbf920
louisgeek <louisgeek@qq.com>
2016-05-17 19:05:14 +0800

    Initial commit

---
+++ app/src/main/java/com/louisgeek/louisviewflipperdemo/MainActivity.java
@@ -0,0 +1,81 @@
+package com.louisgeek.louisviewflipperdemo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ViewFlipper;
+
+public class MainActivity extends AppCompatActivity {
+
+    private final static int INTERVAL= 200;   //最小距离
+    ViewFlipper idvf;
+
+    //手指按下的点为(x1, y1)手指离开屏幕的点为(x2, y2)
+    float x1 = 0;
+    float x2 = 0;
+    float y1 = 0;
+    float y2 = 0;
+
+    /**
+     * setInAnimation：设置View进入屏幕时使用的动画
+     setOutAnimation：设置View退出屏幕时使用的动画
+     showNext：调用该方法来显示ViewFlipper里的下一个View
+     showPrevious：调用该方法来显示ViewFlipper的上一个View
+     setFilpInterval：设置View之间切换的时间间隔
+     startFlipping：使用上面设置的时间间隔来开始切换所有的View，切换会循环进行
+     stopFlipping：停止View切换
+
+     * @param savedInstanceState
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+         idvf = (ViewFlipper) findViewById(R.id.id_vf);
+
+       View view04=LayoutInflater.from(this).inflate(R.layout.view_flipper_04,null);
+        idvf.addView(view04);
+        //idvf.startFlipping();
+
+
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        switch (event.getAction()){
+            case  MotionEvent.ACTION_DOWN:
+                //当手指按下的时候
+                x1 = event.getX();
+                y1 = event.getY();
+                break;
+            case  MotionEvent.ACTION_UP:
+                //当手指离开的时候
+                x2 = event.getX();
+                y2 = event.getY();
+                if(y1 - y2 > INTERVAL) {
+                    //Toast.makeText(MainActivity.this, "向上滑", Toast.LENGTH_SHORT).show();
+                } else if(y2 - y1 > INTERVAL) {
+                    //Toast.makeText(MainActivity.this, "向下滑", Toast.LENGTH_SHORT).show();
+                } else if(x1 - x2 > INTERVAL) {
+                    idvf.setInAnimation(this,R.anim.left_in);
+                    idvf.setOutAnimation(this,R.anim.left_out);
+                    idvf.showNext();
+                    //Toast.makeText(MainActivity.this, "向左滑", Toast.LENGTH_SHORT).show();
+                } else if(x2 - x1 > INTERVAL) {
+                    idvf.setInAnimation(this,R.anim.right_in);
+                    idvf.setOutAnimation(this,R.anim.right_out);
+                    idvf.showPrevious();
+                    //Toast.makeText(MainActivity.this, "向右滑", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            case  MotionEvent.ACTION_MOVE:
+                break;
+            default:
+                break;
+
+        }
+        return super.onTouchEvent(event);
+    }
+}
---
+++ app/src/main/java/com/louisgeek/louisviewflipperdemo/MySimpleOnGestureListener.java
@@ -0,0 +1,64 @@
+package com.louisgeek.louisviewflipperdemo;
+
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+
+/**
+ * Created by louisgeek on 2016/5/17.
+ */
+public class MySimpleOnGestureListener  extends GestureDetector.SimpleOnGestureListener {
+
+    public MySimpleOnGestureListener() {
+        super();
+    }
+
+    @Override
+    public boolean onSingleTapUp(MotionEvent e) {
+        return super.onSingleTapUp(e);
+    }
+
+    @Override
+    public void onLongPress(MotionEvent e) {
+        super.onLongPress(e);
+    }
+
+    @Override
+    public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+        return super.onScroll(e1, e2, distanceX, distanceY);
+    }
+
+    @Override
+    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+        return super.onFling(e1, e2, velocityX, velocityY);
+    }
+
+    @Override
+    public void onShowPress(MotionEvent e) {
+        super.onShowPress(e);
+    }
+
+    @Override
+    public boolean onDown(MotionEvent e) {
+        return super.onDown(e);
+    }
+
+    @Override
+    public boolean onDoubleTap(MotionEvent e) {
+        return super.onDoubleTap(e);
+    }
+
+    @Override
+    public boolean onDoubleTapEvent(MotionEvent e) {
+        return super.onDoubleTapEvent(e);
+    }
+
+    @Override
+    public boolean onSingleTapConfirmed(MotionEvent e) {
+        return super.onSingleTapConfirmed(e);
+    }
+
+    @Override
+    public boolean onContextClick(MotionEvent e) {
+        return super.onContextClick(e);
+    }
+}
